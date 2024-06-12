43fdf2f7c44b31ab24eca87d314b41626e356b03
Basith Ahmed N A <basithahmed.dev@gmail.com>
2024-06-13 00:44:09 +0530

    navbar skeleton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import NavBar from "@/components/global/NavBar";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
+      <NavBar />
     </main>
   );
 }

2703bc790305ce5848bb8ed11f40a52d3f5f4c0a
Basith Ahmed N A <basithahmed.dev@gmail.com>
2024-06-14 01:59:43 +0530

    update: settings page

--- src/components/forms/profile-form.tsx
+++ src/components/forms/profile-form.tsx
@@ -1,11 +1,89 @@
-import React from 'react'
+"use client";
 
-type Props = {}
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { Loader2 } from "lucide-react";
+
+const formSchema = z.object({
+  name: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  email: z.string().email({
+    message: "Please enter a valid email address.",
+  }),
+});
+
+type Props = {};
 
 const ProfileForm = (props: Props) => {
-  return (
-    <div>ProfileForm</div>
-  )
-}
+  const [isLoading, setIsLoading] = useState(false);
 
-export default ProfileForm
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+    },
+  });
+
+  return (
+    <Form {...form}>
+      <form className="flex flex-col gap-6" onSubmit={() => {}}>
+        <FormField
+          disabled={isLoading}
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-lg">User's Name</FormLabel>
+              <FormControl>
+                <Input placeholder="Full Name" {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          disabled={true}
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-lg">E-Mail</FormLabel>
+              <FormControl>
+                <Input placeholder="E-Mail" type="email" {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <Button
+          type="submit"
+          className="self-start hover:bg-[#2F006B] hover:text-white"
+        >
+          {isLoading ? (
+            <>
+              <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+              Saving
+            </>
+          ) : (
+            "Save User Settings"
+          )}
+        </Button>
+      </form>
+    </Form>
+  );
+};
+
+export default ProfileForm;
